ddee15669c33729ca5d45e97e119c2a5f436070d
Believe Nosakhare <believehsam@gmail.com>
2025-04-30 12:48:23 +0530

    initial

---
+++ main.py
@@ -0,0 +1,15 @@
+# Brand Name Generator
+run = input("Type 'yes' to run this code: ").lower()
+
+# Check if the user agreed to run
+if run == 'yes':
+    print("Welcome to the Brand Name Generator!💯")
+
+    city = input("What city did you grow up in?")
+    pet = input("What is your pet's name? ")
+
+
+    print(f"Your brand name could be: {city} {pet}😊")
+else:
+    print("You chose not to run the code. TERMINATED😩")
+
